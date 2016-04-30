c8089982d65b7e684c2a7d84d30bd36aea066f5c
waitig <waitig@hotmail.com>
2016-04-30 21:52:37 +0800

    update

--- spider_crust.py
+++ spider_crust.py
@@ -2,91 +2,74 @@
 
 import time,re,os
 from spider_engine import *
-from make_chm import *
+from save_chm import *
 import sys
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
 
 #----------- 爬取csdn用户所有文章 -----------
-class Spider_Crust:
+class SpiderModel:
 
-	def __init__(self,headers):
+	def __init__(self,headers,index_url,work_home):
 		self.url_list=[]
-		self.post_list=[]
-		self.blog=''
-		self.url=''
-		self.path=''
 		self.dely=0.3
-		self.partlyNum=50
-		self.work_home='cnblogs'
+		self.work_home=work_home
 		self.maxPage=1
 		self.pageNum=1
-		self.index_url='http://www.cnblogs.com/'
-		self.se=Spider_Engine()
-		self.se.set_headers(headers)
-		
-	def get_url_list(self):
-		return self.url_list
-	def get_post_list(self):
-		return self.post_list
-	def get_blog(self):
-		return self.blog
-	def get_path(self):
-		return self.path
-	def set_dely(self,dely):
-		self.dely=dely
-	def set_work_home(self,work_home):
-		self.work_home=work_home
-	def set_index_url(self,index_url):
 		self.index_url=index_url
-	def set_max_page(self,max_page):
-		self.maxPage=max_page
-	def set_page_num(self,page_num):
-		self.pageNum=page_num
-	def clear_url_list(self):
-		del self.url_list[:]
-	def clear_post_list(self):
-		del self.post_list[:]
+		self.se=Spider_Engine(headers)
+		self.user_index_reStr={}
+		self.page_url_reStr={}
+		self.index_url_reStr={}
+		self.post_reStr={}
 		
-
-	#Get urls from the user index
-	def get_user_index_urls(self,url,restr):
+	#Set and get
+	def set_user_index_reStr(self,reStr):
+		self.user_index_reStr=reStr
+	def set_page_url_reStr(self,reStr):
+		self.page_url_reStr=reStr
+	def set_index_url_reStr(self,reStr):
+		self.index_url_reStr=reStr
+	def set_post_reStr(self,reStr):
+		self.post_reStr=reStr
+		
+	#Get urls from the page
+	#type:1--user index;2--other page
+	def get_urls(self,url,type,flag=0):
+		if(type==1):
+			self.get_user_index_urls(url,type)
+			return
+		else:
+			self.get_page_urls(url,type)
+			return
+		
+	#Get user index urls
+	def get_user_index_urls(self,url,type,mcm,flag=0):
 		urlInfo['urlList']=[]
 		urlInfo['nextUrl']=''
 		urlInfo['title']=''
-		urlInfo=self.se.get_urls(url,reStr)
-		for n in urlInfo['urlList']:
-			url=self.index_url+n
-			self.url_list.append(url)
-		
+		urlInfo=self.se.get_urls(url,self.user_index_reStr,self.index_url)
+		self.url_list+=urlInfo['urlList']
+		if(flag==0):
+			blog=self.get_blog(1,urlInfo['title'])
+			path=self.get_path(1,url)
+			mcm.set_para(self.work_home,blog,path)
 		if urlInfo['nextUrl']:
-			urls=self.index_url+urlInfo['nextUrl']
-			print 'Loading url:'+urls
+			print 'Loading url:'+urlInfo['nextUrl']
 			time.sleep(self.dely)
-			self.get_urls(urls,1)
+			self.get_user_index_urls(urlInfo['nextUrl'],1,1)
 		else:
-			blog=urlInfo['title'].split('-')
-			pos=len(blog)-2
-			self.blog=self.work_home+'_'+blog[pos]
-			self.path=url.split('/')[3]
-			return True
+			return
 
 	#获取任意页面可爬取的url列表
-	def get_page_urls(self,url,reStr):
+	def get_page_urls(self,url,type,mcm,flag=0):
 		print 'Loadding page [ '+str(self.pageNum)+' ] : '+url
-		tm=time.strftime('%y_%m_%d',time.localtime(time.time()))
-		urlInfo=self.se.get_urls(url,reStr)
-		trueUrl=url.replace(self.index_url,'')
-		trueUrl=trueUrl.replace('//','/').replace('?','_').replace('&','_').replace('.','_')
-		self.path=(trueUrl.replace('/','_')+'_'+tm).replace('__','_')
-		try:
-			title=urlInfo['title'].split(' - ')
-			self.blog=(title[1]+'_'+title[0]+'_'+tm).decode('utf-8')
-		except UnicodeDecodeError:
-			self.blog=self.path
-
-		for n in urlInfo['urlList']:
-			self.url_list.append(n)
+		urlInfo=self.se.get_urls(url,self.page_url_reStr)
+		if(flag==0):
+			path=self.get_path(type,url)
+			blog=self.get_blog(type,urlInfo['title'])
+			mcm.set_para(self.work_home,blog,path)
+		self.url_list+=urlInfo['urlList']
 		#Recursive
 		self.pageNum+=1
 		if(self.pageNum>self.maxPage and self.maxPage!=-1):
@@ -94,89 +77,78 @@ class Spider_Crust:
 			return
 		else:
 			if urlInfo['nextUrl']:
-				self.get_page_urls(self.index_url+urlInfo['nextUrl'])
+				self.get_page_urls(urlInfo['nextUrl'],type,1)
 			else:
 				self.pageNum=1
 				return
-
+			
 	#从主页开始获取所有频道首页文章
-#	def get_index_data(self,mcm):
-#		self.pageNum=1
-#		#deal index posts
-#		self.get_urls(self.index_url,2)
-#		self.get_posts(mcm)
-#		#deal categroy posts
-#		reStr = {
-#		'urlList':'href=\"(?P<path_list>/cate/\d+\/)\"',
-#		'nextUrl':'',
-#		'title':'<title>(?P<path_list>.*?)</title>'
-#		}
-#		urlInfo=self.se.get_urls(self.index_url,reStr)
-#		for url in urlInfo['urlList']:
-#			url=self.index_url+url
-#			print 'Find url: '+url
-#			self.get_urls(url,2)
-#			self.get_posts(mcm)
-
+	def get_index_data(self,mcm):
+		self.pageNum=1
+		#deal index posts
+		self.get_urls(self.index_url,3,mcm)
+		self.get_posts(mcm)
+		#deal categroy posts
+		urlInfo=self.se.get_urls(self.index_url,self.index_url_reStr)
+		for url in urlInfo['urlList']:
+			print 'Find url: '+url
+			self.get_page_urls(url,2)
+			self.get_posts(mcm)
 
 	#获取文章
 	def get_posts(self,mcm):
 		self.url_list=list(set(self.url_list))
 		print 'Total '+str(len(self.url_list))+' articles found , start loading.'
 		num=1
-		part=1
-		partly=0
 		for n in self.url_list:
 			print 'NO ['+str(num)+'] Loading URL:'+n
-			self.get_post(n)
-			num+=1
-			if(num>self.partlyNum):
-				partly=1
-				print 'Get '+str(self.partlyNum)+' articles,start to create html'
-				self.save_html_partly(mcm,part,partly)
-				part+=1
-				num=1
+			post=self.se.get_post(url,self.post_reStr)
+			post['title']=self.deal_post_title(post['title'])
+			post['content']=self.deal_post_content(post['content'])
+			mcm.save_post(post)
 			time.sleep(self.dely)
-		self.save_html_partly(mcm,part,partly)
 		del self.url_list[:]
-
-	#根据链接获取文章相关信息
-	def get_post(self,url,restr):
-		post=self.se.get_post(url,reStr)
-		self.post_list.append(post)
-
-
-	def save_html_partly(self,mcm,part,partly):
-		#mcm=MakeChm()
-		if(partly>0):
-			blog=self.blog+'_part_'+str(part)
-			path=self.path+'_part_'+str(part)
-		else:
-			blog=self.blog
-			path=self.path
-		mcm.set_para(self.work_home,blog,path,self.post_list)
-		mcm.save()
-		del self.post_list[:]
+		
+	#Return the file name in client version
+	def get_blog(self,type,title):
+		titles=title.split('-')
+		fileName="cnblogs_".join(titles[0:(len(titles)-2)])
+		if(type!=1):
+			tm=time.strftime('%y_%m_%d',time.localtime(time.time()))
+			fileName=fileName+'_'+tm
+		return fileName
+	#Return the direction name in all version
+	#type--1:user index --2:other page --3:blog index
+	def get_path(self,type,url):
+		if(type!=3):
+			url=url.replace(self.index_url,'')
+		url=(url.replace('//','/')).strip('/')
+		url=url.replace('?','_').replace('&','_').replace('.','_')
+		path=(url.replace('__','_')).strip('_')
+		if(type!=1):
+			tm=time.strftime('%y_%m_%d',time.localtime(time.time()))
+			path=path+'_'+tm
+		return path
+	#Return the post titles 
+	def deal_post_title(self,title):
+		titles=title.split('-')
+		return "".join(titles[0:(len(titles)-2)])
+	def deal_post_content(self,content):
+		return content
 
 	def set_dely(self,dely):
 		self.dely=dely
 
-	def set_partlyNum(self,partlyNum):
-		self.partlyNum=partlyNum
-
 	def set_max_page(self,maxPage):
 		self.maxPage=maxPage
-
-	def set_index_url(self,index_url):
-		self.index_url=index_url
 
 if __name__ == '__main__':
 	sc=Spider_Cnblogs()
 	mcm=MakeChm()
 	mcm.set_save_img(0)
+	mcm.set_partlyNum(20)
 	mcm.set_chm_path('E:\pycode\spider\chm\cnblogs_host')
 	sc.set_dely(1)
 	sc.set_max_page(2)
 	sc.get_index_data(mcm)
 	#sc.get_posts(mcm)
-
--- spider_engine.py
+++ spider_engine.py
@@ -1,19 +1,32 @@
 # -*- coding: utf-8 -*-
 #This is the heart of this spider
 
-import requests,time,re,os
+import requests,time,re,os,urllib2,urllib,cookielib
 
 class Spider_Engine:
 	def __init__(self,headers):
 		self.debug=1
 		self.headers = headers
 		self.se = requests.Session()
+		self.useUrllib2=0
+		self.usePost=0
+		self.postData={}
+		# 初始化一个CookieJar来处理Cookie
+		cookieJar=cookielib.CookieJar()
+		# 实例化一个全局opener
+		self.opener=urllib2.build_opener(urllib2.HTTPCookieProcessor(cookieJar))
 	def set_debug(self,debug):
 		self.debug=debug
 	def set_headers(self,headers):
 		self.headers=headers
 	def get_title(self):
 		return self.title
+	def set_useUrllib2(self,used):
+		self.useUrllib2=used
+	def set_usePost(self,used):
+		self.usePost=used
+	def set_postData(self,postData):
+		self.postData=postData
 	def dpt(self,str):
 		if(self.debug):
 			print str
@@ -23,18 +36,30 @@ class Spider_Engine:
 	#Get urls in the page
 	def get_urls(self,url,reStr,pre_url=''):
 		urlInfo = {'title':'','urlList':[],'nextUrl':''}
-		Reslut=self.se.get(url,headers=self.headers)
-		urlList=re.findall(reStr['urlList'],Reslut.text)
+		text=''
+		if(self.useUrllib2):
+			response = urllib2.urlopen(url)
+			text=response.read()
+		else:
+			try:
+				if(self.usePost):
+					Reslut=self.se.post(url,postData,self.headers,verify=True)
+				else:
+					Reslut=self.se.get(url,headers=self.headers)
+			except requests.exceptions.TooManyRedirects:
+				print 'This article not found [404]!'
+			text=Reslut.text
+		urlList=re.findall(reStr['urlList'],text)
 		urlList=list(set(urlList))
 		for n in urlList:
-			urlInfo['urlList'].append(pre_url+n)
+			urlInfo['urlList'].append(pre_url+n.strip('/'))
 		#urlInfo['urlList']=urlList
-		title=re.findall(reStr['title'],Reslut.text)
+		title=re.findall(u'<title>(?P<title>[\s\S]*?)</title>',text)
 		if(title!=[]):
-			urlInfo['title']=title[0]
-		nextUrl=re.findall(reStr['nextUrl'],Reslut.text)
+			urlInfo['title']="".join(title[0].split())
+		nextUrl=re.findall(reStr['nextUrl'],text)
 		if(nextUrl!=[]):
-			urlInfo['nextUrl']=pre_url+nextUrl[0]
+			urlInfo['nextUrl']=pre_url+nextUrl[0].strip('/')
 		return urlInfo
 	
 	#Get post from the url
@@ -46,24 +71,39 @@ class Spider_Engine:
 		post['keywords']=[]
 		post['categories']=[]
 		post['content']=''
-		try:
-			Reslut=self.se.get(url,headers=self.headers)
-		except requests.exceptions.ConnectionError:
-			print 'This article not found [404]!'
-			post['title']='404 NOT FOUND!'
-			post['content']='404 NOT FOUND!'
-			return post
-		text=Reslut.text
-		title=re.findall('<title>(?P<path_list>.*?)</title>',text,re.S)
+		text=''
+		if(self.useUrllib2):
+			response = urllib2.urlopen(url)
+			text=response.read()
+		else:
+			try:
+				Reslut=self.se.get(url,headers=self.headers)
+			except requests.exceptions.ConnectionError:
+				print 'This article not found [404]!'
+				post['title']='404 NOT FOUND!'
+				post['content']='404 NOT FOUND!'
+				return post
+			except requests.exceptions.TooManyRedirects:
+				print 'This article not found [404]!'
+				post['title']='404 NOT FOUND!'
+				post['content']='404 NOT FOUND!'
+				return post
+			text=Reslut.text
+		#print type(text)
+		#print text.decode('gbk')
+		title=re.findall(u'<title>(?P<title>[\s\S]*?)</title>',text,re.S)
 		try:
 			post['title']="".join(title[0].split())
 		except UnicodeDecodeError:
 			print 'Get title unicode decode error!'
 		id=re.findall(reStr['id'],url)
-		try:
-			post['id']=id[0]
-		except UnicodeDecodeError:
-			print 'Get ID unicode decode error!'
+		#print url
+		#print reStr['id']
+		if(id!=[]):
+			try:
+				post['id']=id[0]
+			except UnicodeDecodeError:
+				print 'Get ID unicode decode error!'
 		keywords=re.findall(reStr['keywords'],text)
 		for n in keywords:
 			post['keywords'].append(n)
@@ -71,15 +111,43 @@ class Spider_Engine:
 		for n in categories:
 			post['categories'].append(n)
 		content=re.findall(reStr['content'],text,re.S)
-		#self.dpt(reStr['content'])
-		#self.dpt(post)
+		tmp=1
+		while(content==[]):
+			contentKey='content'+str(tmp)
+			try:
+				content=re.findall(reStr[contentKey],text,re.S)
+			except KeyError:
+				content=['Cant find the content!']
+				break
+			tmp+=1
 		try:
 			#post['content']=re.sub('<\s*script[^>]*>[^<]*<\s*/\s*script\s*>','',content[0])
 			post['content']=content[0]
 		except UnicodeDecodeError:
 			print 'Get content unicode decode error!'
 		return post
-		
+	
+	def get_others(self,url,headers,useUrllib=0,usePost=0,postData={}):
+		if(useUrllib):
+			print 'Use urllib'
+			postData = urllib.urlencode(postData)
+			print headers
+			req = urllib2.Request(url, postData, headers)
+			response = self.opener.open(req)
+			#response = urllib2.urlopen(req)
+			text=response.read()
+		else:
+			try:
+				if(usePost):
+					print 'Use post'
+					Reslut=self.se.post(url,postData,headers,verify=True)
+				else:
+					print 'Use get'
+					Reslut=self.se.get(url,headers=self.headers)
+			except requests.exceptions.TooManyRedirects:
+				print 'This article not found [404]!'
+			text=Reslut
+		return text
 		
 		
 if __name__ == '__main__':
